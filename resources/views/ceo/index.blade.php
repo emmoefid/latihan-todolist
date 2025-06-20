2520472d33d2cb49805c55429a4c74efe9fa8629
emmoefid <muhammadmofed123@gmail.com>
2025-06-20 13:34:15 +0700

    benerin

--- resources/views/ceo/index.blade.php
+++ resources/views/ceo/index.blade.php
@@ -70,12 +70,6 @@
                                 <a href="/admin/ubah-tugas/{{ $todo->id_todo }}" class="text-yellow-600 hover:underline">Ubah</a>
                                 <a href="/admin/hapus-tugas/{{ $todo->id_todo }}" onclick="return confirm('Apakah Anda yakin ingin menghapus tugas ini?')" class="text-red-600 hover:underline">Hapus</a>
                             </td>
-                            </td>
-                            <td class="px-4 py-2 text-center space-x-2">
-                                <a href="/ceo/detail-tugas/{{ $todo->id_todo }}" class="text-blue-600 hover:underline">Detail</a>
-                                <a href="/ceo/ubah-tugas/{{ $todo->id_todo }}" class="text-yellow-600 hover:underline">Ubah</a>
-                                <a href="/ceo/hapus-tugas/{{ $todo->id_todo }}" onclick="return confirm('Apakah Anda yakin ingin menghapus tugas ini?')" class="text-red-600 hover:underline">Hapus</a>
-                            </td>
                         </tr>
                         @endforeach
                     </tbody>
